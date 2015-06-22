ed6fc09d47c7f748f868577bf1950d9f2d270119
Tomislav Rakic <tomislavr@nordeus.eu>
2015-06-22 23:06:06 +0200

    Added Profiling.

---
+++ Profiling/ProfilerFactory.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Usage:
+/// using(ProfilerFactory.GetProfiler("profilerName"))
+/// {
+///		ExpensiveMethod();
+///		// more code that you want to profile quickly
+/// }
+/// 
+/// 
+/// Add a call to ProfilerFactory.DumpProfilerInfo(); to log all measured data.
+/// </summary>
+public static class ProfilerFactory
+{
+	#region Constants
+
+	private const string LogFormat = "Profiler {0} - Total: {1}ms | Self - {2}ms | Number of calls - {3}";
+
+	#endregion
+
+	#region Static
+
+	private static Dictionary<string, StopwatchProfiler> profilers = new Dictionary<string, StopwatchProfiler>();
+
+	public static readonly bool DoNotProfile = false;
+
+	#endregion Static
+
+	#region Methods
+
+	public static StopwatchProfiler GetProfiler(string name)
+	{
+		if (DoNotProfile) return null;
+
+		StopwatchProfiler profiler;
+		profilers.TryGetValue(name, out profiler);
+
+		if (profiler == null)
+		{
+			profiler = new StopwatchProfiler();
+			profilers.Add(name, profiler);
+		}
+
+		return profiler;
+	}
+
+	public static StopwatchProfiler GetAndStartProfiler(object name)
+	{
+		if (DoNotProfile) return null;
+		StopwatchProfiler profiler = GetProfiler(name.ToString());
+		profiler.Start();
+		return profiler;
+	}
+
+	public static void DumpProfilerInfo(float timeThresholdToLog = 0f, bool resetProfilers = true)
+	{
+		foreach (var pair in profilers)
+		{
+			pair.Value.ForceStop();
+		}
+
+		var list = new List<KeyValuePair<string, StopwatchProfiler>>();
+
+		foreach (var pair in profilers)
+		{
+			list.Add(pair);
+		}
+
+		list.Sort(delegate(KeyValuePair<string, StopwatchProfiler> a, KeyValuePair<string, StopwatchProfiler> b) { return a.Value.ElapsedMilliseconds.CompareTo(b.Value.ElapsedMilliseconds); });
+
+		double fullTime = 0;
+		foreach (var pair in list)
+		{
+			double time = pair.Value.ElapsedMillisecondsSelf;
+			fullTime += time;
+			if (pair.Value.NumberOfCalls > 0 && time > timeThresholdToLog) 
+			{
+				Debug.Log(string.Format(LogFormat, pair.Key, pair.Value.ElapsedMilliseconds, pair.Value.ElapsedMillisecondsSelf.ToString("0.00"), pair.Value.NumberOfCalls));
+			}
+			if (resetProfilers) pair.Value.Reset();
+		}
+
+		if (fullTime > timeThresholdToLog) { UnityEngine.Debug.Log("Full stopwatch measured(ms): " + fullTime); }
+	}
+
+	#endregion Methods
+}
---
+++ Profiling/StopwatchProfiler.cs
@@ -0,0 +1,100 @@
+﻿	using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class StopwatchProfiler : System.IDisposable
+{
+	#region Static
+	public static readonly Stack<StopwatchProfiler> ProfilerStack = new Stack<StopwatchProfiler>();
+
+	/// <summary>
+	/// Initializes static profiler stack with a sentry null, allows for peeking when stack is empty.
+	/// </summary>
+	static StopwatchProfiler()
+	{
+		ProfilerStack.Push(null);
+	}
+
+	#endregion Static
+
+	#region Fields
+
+	private Stopwatch stopWatch = new Stopwatch();
+
+	private int nestingLevel = 0;
+
+	private double childrenElapsedMilliseconds = 0f;
+
+	private double lastElapsedMilliseconds = 0f;
+
+	public StopwatchProfiler()
+	{
+		NumberOfCalls = 0;
+	}
+
+	#endregion Fields
+
+	#region Properties
+
+	public double ElapsedMilliseconds
+	{
+		get { return stopWatch.Elapsed.TotalMilliseconds; }
+	}
+
+	public double ElapsedMillisecondsSelf
+	{
+		get { return ElapsedMilliseconds - childrenElapsedMilliseconds; }
+	}
+
+	public int NumberOfCalls { get; private set; }
+
+	#endregion Properties
+
+	public void Start()
+	{
+		StopwatchProfiler lastProfiler = ProfilerStack.Peek();
+		if (lastProfiler != this) ProfilerStack.Push(this);
+
+		nestingLevel++;
+		NumberOfCalls++;
+
+		if (nestingLevel == 1) stopWatch.Start();
+	}
+
+	public void Stop()
+	{
+		if (nestingLevel == 1) 
+		{
+			stopWatch.Stop();
+
+			StopwatchProfiler previousProfiler = ProfilerStack.Peek();
+			if (previousProfiler == this) { ProfilerStack.Pop(); }
+
+			previousProfiler = ProfilerStack.Peek();
+			if (previousProfiler != null) previousProfiler.childrenElapsedMilliseconds += (ElapsedMilliseconds - lastElapsedMilliseconds);
+			lastElapsedMilliseconds = ElapsedMilliseconds;
+		}
+		nestingLevel--;
+	}
+
+	public void ForceStop()
+	{
+		stopWatch.Stop();
+		nestingLevel = 0;
+	}
+
+	public void Reset()
+	{
+		stopWatch.Reset();
+		nestingLevel = 0;
+		NumberOfCalls = 0;
+		childrenElapsedMilliseconds = 0f;
+		lastElapsedMilliseconds = 0f;
+	}
+
+	public void Dispose()
+	{
+		Stop();
+	}
+}
